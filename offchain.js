6d027a67bcf78839ff138cdd2e926f1f8ca7c85e
LayneHaber <layne.haber@gmail.com>
2021-04-28 22:36:16 -0600

    fix

--- offchain.js
+++ offchain.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const { getBalanceForAssetId } = require("@connext/vector-utils");
 const { readFileSync, createWriteStream } = require("fs");
 const { BigNumber, constants, providers, utils } = require("ethers");
