73b1fa448fa8e1f8197af77d0a18f5da8b45c464
Rahul Sethuram <rahul@sethuram.com>
2021-05-10 18:24:03 +0400

    Submit

--- input/xdai.ts
+++ input/xdai.ts
@@ -1,7 +1,9 @@
 export default {
   unsubmitted: {
     router: [
-      "0xf1fc5e03bfca41fb610fefddfa661d8bbc205e1ee3080880ad9b81119d334b4b",
+      "0xba7a2a2b906a7bf8c351eeb7236d37ba0f3ba39af0beebef9950ed25ca09e6b9",
+      "0xdc8bfe150979fbcef25c02ea82376b5e321a8a0b770e98b515c4090627f5b827",
+      "0x234dfdd5f7a7caa5e787272472c3b1640c239fc0e565b995ff70d656cf1bd110",
     ],
     user: [],
   },
