2bbff6d77954bf42da55b5e3e3eadb4776380a32
Jake Kidd <jcb.kdd@gmail.com>
2021-06-11 22:49:01 -0700

    fix query

--- query.ts
+++ query.ts
@@ -9,8 +9,9 @@ export const QUERY = {
 export const sendQuery = async (query: string): Promise<string> => {
   return new Promise((resolve, reject) => {
     try {
+      query = query.replace(/"/g, `\\\\"`);
       const result = execSync(
-        `docker exec -t db-node bash -c "psql vector --username=vector -c \"select transfer."amountA", transfer."amountB", update."assetId", channel."chainId" from update join transfer on transfer."transferId" = update."transferId" join channel on update."channelAddressId" = channel."channelAddress" where update."transferDefinition" = '0xed911640fd86f92fD1337526010adda8F3Eb8344' and "transferResolver" is null and "fromIdentifier" = 'vector52rjrwRFUkaJai2J4TrngZ6doTUXGZhizHmrZ6J15xVv4YFgFC' limit = 1;\""`,
+        `docker exec -t db-node bash -c $'psql vector --username=vector -c "${query}" -x'`,
         {
           stdio: ["ignore", "ignore", "pipe"],
         }
@@ -22,6 +23,7 @@ export const sendQuery = async (query: string): Promise<string> => {
       }
     } catch (e) {
       console.error(e);
+      reject(e);
     }
   });
 
