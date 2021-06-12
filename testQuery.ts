5f96b3526a2655633e04083ecb6db29f9d42bfe2
Jake Kidd <jcb.kdd@gmail.com>
2021-06-11 17:43:40 -0700

    retry simpler

--- testQuery.ts
+++ testQuery.ts
@@ -3,19 +3,21 @@ import { ChildProcessWithoutNullStreams, spawn } from "child_process";
 // const query = `select transfer."transferId", channel."channelAddress" from update join transfer on transfer."transferId" = update."transferId" join channel on update."channelAddressId" = channel."channelAddress" where update."transferDefinition" = '0xed911640fd86f92fD1337526010adda8F3Eb8344' and "transactionHash" is null and channel."chainId" = '56' and "fromIdentifier" = 'vector52rjrwRFUkaJai2J4TrngZ6doTUXGZhizHmrZ6J15xVv4YFgFC';`;
 const COMMAND = {
   BASE: `docker exec -t db-node bash -c "psql vector --username=vector"`,
-  EXPANDED_DISPLAY: "\\x\n",
+  EXPANDED_DISPLAY: "hello\n",
 };
 
 const execCommand = (
   child: ChildProcessWithoutNullStreams,
   command: string
 ) => {
+  console.log("Writable?", child.stdin.writable);
   const writeCommand = command + "\r\n";
   console.log("Writing", writeCommand);
   child.stdin.write(writeCommand, (error) => {
-    console.error("Error in writing:", error);
+    if (error) {
+      console.error("Error in writing:", error);
+    }
   });
-  child.stdin.end();
 };
 
 const query = `select * from update limit 1;`;
@@ -37,9 +39,9 @@ child.stdout.on("data", function(data) {
     case 1:
       execCommand(child, COMMAND.EXPANDED_DISPLAY);
       break;
-    case 2:
-      execCommand(child, query);
-      break;
+    // case 2:
+    //   execCommand(child, query);
+    //   break;
     default:
       child.kill();
       break;
