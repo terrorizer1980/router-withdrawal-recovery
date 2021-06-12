457e52b1c9460b405e212b84fe424f6f5af25966
Jake Kidd <jcb.kdd@gmail.com>
2021-06-11 23:22:22 -0700

    query fixes

--- query.ts
+++ query.ts
@@ -14,8 +14,10 @@ export const sendQuery = async (query: string): Promise<string> => {
       const command = `docker exec -t db-node bash -c $'psql vector --username=vector -c \\"${query}\\" -x'`;
       console.log("Sending command:\n", command, "\n");
       const result = execSync(command, {
-        stdio: ["inherit", "pipe", "inherit"],
+        // stdio: ["inherit", "pipe", "inherit"],
         shell: "/bin/bash",
+        encoding: "utf8",
+        maxBuffer: 50 * 1024 * 1024,
       });
       if (result) {
         console.log(result.toString());
