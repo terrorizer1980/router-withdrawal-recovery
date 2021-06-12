6b2198b62f28844319074ad51e0bff84ba53a384
Jake Kidd <jcb.kdd@gmail.com>
2021-06-11 23:42:32 -0700

    query fixes

--- query.ts
+++ query.ts
@@ -2,7 +2,7 @@ import { exec, spawnSync, execSync } from "child_process";
 
 export const QUERY = {
   GET: {
-    SINGLE_SIGNED: `select transfer."amountA", transfer."amountB", update."assetId", channel."chainId" from update join transfer on transfer."transferId" = update."transferId" join channel on update."channelAddressId" = channel."channelAddress" where update."transferDefinition" = '0xed911640fd86f92fD1337526010adda8F3Eb8344' and "transferResolver" is null and "fromIdentifier" = 'vector52rjrwRFUkaJai2J4TrngZ6doTUXGZhizHmrZ6J15xVv4YFgFC' limit 10;`,
+    SINGLE_SIGNED: `select transfer."amountA", transfer."amountB", update."assetId", channel."chainId" from update join transfer on transfer."transferId" = update."transferId" join channel on update."channelAddressId" = channel."channelAddress" where update."transferDefinition" = '0xed911640fd86f92fD1337526010adda8F3Eb8344' and "transferResolver" is null and "fromIdentifier" = 'vector52rjrwRFUkaJai2J4TrngZ6doTUXGZhizHmrZ6J15xVv4YFgFC' limit 100;`,
   },
 };
 
@@ -14,20 +14,12 @@ export const sendQuery = async (query: string): Promise<string> => {
       const command = `docker exec db-node bash -c $'psql vector --username=vector -c \\"${query}\\" -x'`;
       console.log("Sending command:\n", command, "\n");
 
-      // let stdout = "";
-      // process.stdout.on("data", (data) => {
-      //   stdout += data;
-      // });
       const result = execSync(command, {
-        // stdio: "ignore",
-        // stdio: ["inherit", "inherit", "inherit"],
         shell: "/bin/bash",
         encoding: "utf8",
         maxBuffer: 50 * 1024 * 1024,
         timeout: 10000,
       });
-      // console.log("\n\n\n**********STDOUT CAPTURED:\n", stdout.toString());
-      console.log(result);
       if (result) {
         resolve(result.toString());
       } else {
@@ -37,17 +29,4 @@ export const sendQuery = async (query: string): Promise<string> => {
       console.error(e);
     }
   });
-
-  // let child = exec(
-  //   `docker exec -t db-node bash -c "psql vector --username=vector -c \"${query}\" -x"`
-  // );
-  // child.stderr.on("data", function(data) {
-  //   console.error("STDERR:", data.toString());
-  // });
-  // child.stdout.on("data", function(data) {
-  //   const stdout: string = data.toString();
-  // });
-  // child.on("exit", function(exitCode) {
-  //   console.log("Child exited with code: " + exitCode);
-  // });
 };
