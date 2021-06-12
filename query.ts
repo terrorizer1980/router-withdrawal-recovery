7a11bd2ae747ba3d225727a927d2242bd8452cf6
Jake Kidd <jcb.kdd@gmail.com>
2021-06-11 23:00:38 -0700

    query fixes

--- query.ts
+++ query.ts
@@ -19,6 +19,7 @@ export const sendQuery = async (query: string): Promise<string> => {
         }
       );
       if (result) {
+        console.log(result);
         resolve(result.toString());
       } else {
         throw new Error(`Result was not valid: ${result}`);
