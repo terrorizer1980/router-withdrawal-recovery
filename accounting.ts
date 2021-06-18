440f8c2153ed454fa04688ba7287dace41637c9b
Jake Kidd <jcb.kdd@gmail.com>
2021-06-17 22:21:09 -0700

    fix 'data'

--- accounting.ts
+++ accounting.ts
@@ -2,15 +2,19 @@ import { sendQuery, QUERY } from "./query";
 import { parseGenericQuery, saveJsonFile } from "./utils";
 import { ASSET_MAP } from "./constants";
 
+type SumData = {
+  sum: string;
+};
+
 const run = async () => {
   let sum = 0;
   for (const asset in ASSET_MAP) {
     const info = ASSET_MAP[asset];
     const response = await sendQuery(QUERY.SUM_VALUE(-1, asset), false);
     try {
-      const data = parseGenericQuery(response);
-      const amount = parseFloat(data["sum"]) / Math.pow(10, info.decimals);
-      console.log(data, data["sum"], parseFloat(data["sum"]));
+      const data = parseGenericQuery<SumData>(response)[0];
+      const amount = parseFloat(data.sum) / Math.pow(10, info.decimals);
+      console.log(data, data.sum, parseFloat(data.sum));
       console.log(info.token, ":", amount);
       if (amount) {
         sum += amount;
